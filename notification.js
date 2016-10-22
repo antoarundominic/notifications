be2eb5239dccc15f2d00602e5c0b04897e330583
Anto Arun <antoarundominic@gmail.com>
2016-10-22 16:02:23 +0530

    Initial commit

---
+++ notification.js
@@ -0,0 +1,76 @@
+var GCM_ENDPOINT = 'https://android.googleapis.com/gcm/send';
+
+(function($) {
+  return {
+    init: function() {
+    this.loadServiceWorker();
+    // subscribe();
+  },
+
+  loadServiceWorker: function() {
+    $.getScript( "https://github.com/jaga3421/helloruby/blob/master/sw.js" )
+    .done(function( script, textStatus ) {
+      console.log( textStatus );
+      this.subscribe();
+    })
+    .fail(function( jqxhr, settings, exception ) {
+        $( "div.log" ).text( "Triggered ajaxError handler." );
+    });
+
+  },
+  subscribe: function() {
+    if ('serviceWorker' in navigator) { 
+      navigator.serviceWorker.register('sw.js').then(function() {
+        return navigator.serviceWorker.ready;
+      }).then(function(reg) {
+        console.log('Service Worker is ready)', reg);
+        reg.pushManager.subscribe({userVisibleOnly: true}).then(function(subscription) {
+          var mergedEndpoint = this.endpointWorkaround(subscription);
+          var deviceId = this.getDeviceId(mergedEndpoint);
+          this.registerDevice(deviceId);
+        });
+      }).catch(function(err) {
+        console.log('Service Worker error', error);
+      });
+    }
+  },
+
+  endpointWorkaround: function(pushSubscription) {
+    if (pushSubscription.endpoint.indexOf(GCM_ENDPOINT) !== 0) {
+      return pushSubscription.endpoint;
+    }
+    var mergedEndpoint = pushSubscription.endpoint;
+    if  (this.isSubscriptionIdPresent()) {
+      mergedEndpoint = [pushSubscription.endpoint, '/', pushSubscription.subscriptionId].join('');
+    }
+    return mergedEndpoint;
+  },
+
+  isSubscriptionIdPresent(pushSubscription) {
+    return (pushSubscription.subscriptionId &&
+    pushSubscription.endpoint.indexOf(pushSubscription.subscriptionId) === -1);
+  },
+
+  getDeviceId: function(mergedEndpoint) {
+    if (mergedEndpoint.indexOf(GCM_ENDPOINT) !== 0) {
+      console.log('This browser isn\'t currently ' +
+        'supported for this demo');
+      return mergedEndpoint;
+    }
+    var endpointSections = mergedEndpoint.split('/');
+    var subscriptionId = endpointSections[endpointSections.length - 1];
+  },
+
+  registerDevice: function(deviceId) {
+    var userEmail = domHelper.getAgentEmail();
+    $.ajax({
+      data: {email: userEmail, deviceId: deviceId},
+      url: 'localhost:3000/device',
+      method: 'POST',
+      dataType: 'json',
+      success: function(data){console.log('Registered Successfully!'); }
+    });
+  },
+  this.init()
+  }
+}) (jQuery);
---
+++ sw.js
@@ -0,0 +1,20 @@
+'use strict';
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('sw Installed');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('sw Activated');
+});
+
+self.addEventListener('push', function(event) {
+  console.log('Push message', event);
+  var title = 'Push message';
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      'body': 'The Message',
+      'icon': 'images/icon.png'
+    }));
+});
