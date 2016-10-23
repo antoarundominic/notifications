cedd4dd3b80a3c53ccf942a36e1880424c000da6
Anto Arun <antoarundominic@gmail.com>
2016-10-23 13:13:48 +0530

    try:1 onesignal

--- index.php
+++ index.php
@@ -9,20 +9,20 @@ setTimeout(function() {
   var notification = window.notification;
   console.log("window.notification", notification);
   // window.notification.getDeviceId('test');
-  if ('serviceWorker' in navigator) { 
-      navigator.serviceWorker.register('sw.js').then(function() {
-        return navigator.serviceWorker.ready;
-      }).then(function(reg) {
-        console.log('Service Worker is ready)', reg);
-        reg.pushManager.subscribe({userVisibleOnly: true}).then(function(subscription) {
-          var mergedEndpoint = notification.endpointWorkaround(subscription);
-          var deviceId = notification.getDeviceId(mergedEndpoint);
-          notification.registerDevice(deviceId);
-        });
-      }).catch(function(err) {
-        console.log('Service Worker error', err);
-      });
-    }  
+  // if ('serviceWorker' in navigator) { 
+  //     navigator.serviceWorker.register('sw.js').then(function() {
+  //       return navigator.serviceWorker.ready;
+  //     }).then(function(reg) {
+  //       console.log('Service Worker is ready)', reg);
+  //       reg.pushManager.subscribe({userVisibleOnly: true}).then(function(subscription) {
+  //         var mergedEndpoint = notification.endpointWorkaround(subscription);
+  //         var deviceId = notification.getDeviceId(mergedEndpoint);
+  //         notification.registerDevice(deviceId);
+  //       });
+  //     }).catch(function(err) {
+  //       console.log('Service Worker error', err);
+  //     });
+  //   }  
 }, 5000);
 
 </script>
