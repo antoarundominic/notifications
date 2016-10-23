dc95f400aa8bfc51c00a2e6773a507cbb02b3809
Anto Arun <antoarundominic@gmail.com>
2016-10-23 20:59:20 +0530

    Minion added

--- index.php
+++ index.php
@@ -5,18 +5,81 @@
 <script   src="base64.js"></script>
 <link rel="manifest" href="manifest.json">
 
-<style>
-html {
-   background: #f2f2f2;
-   padding: 50px;
-}
-h3 {
-  color: #444;
-  font-family: Helvetica, sans-serif;
-}
 
-</style>
-
+<!-- Minion html start-->
+<section class="content" id="target">
+          <ul class="hair hair-left">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          </ul>
+          <ul class="hair hair-right">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          </ul>
+          <div class="body">
+          <div class="glasses">
+          <span class="band band-left"></span>
+          <span class="band band-right"></span>
+          <div class="glass">
+           <div class="iris iris-left">
+             <div class="shine"></div>
+           </div>
+          </div>
+          <div class="glass">
+           <div class="iris iris-right">
+             <div class="shine"></div>
+           </div>
+          </div>
+          </div>
+          </div>
+          <div class="mouth">
+          <ul class="teeth">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          </ul>
+          </div>
+          <div class="pants">
+          <div class="belt belt-left"></div>
+          <div class="belt belt-right"></div>
+          </div>
+          <div class="super-pants">
+          <div class="symbol">
+          <div class="s-first-part"></div>
+          <div class="s-second-part"></div>
+          </div>
+          </div>
+          <div class="arm arm-left">
+          <div class="hand">
+          <ul class="fingers fingers-left">
+           <li class="finger"></li>
+           <li class="finger"></li>
+           <li class="finger"></li>
+          </ul>
+          </div>
+          </div>
+          <div class="arm arm-right">
+          <div class="hand">
+          <ul class="fingers fingers-right">
+           <li class="finger"></li>
+           <li class="finger"></li>
+          </ul>
+          </div>
+          </div>
+          <div class="legs">
+          <div class="leg"></div>
+          <div class="leg"></div>
+          </div>
+          <div class="shoes shoes-left"></div>
+          <div class="shoes shoes-right"></div>
+          <div class="coat"></div>
+     </section>
+<!--  Minion html end-->
 
 
 <script type="text/javascript" defer>
