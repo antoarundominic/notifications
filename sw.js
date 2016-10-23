c7e78924ef1b1f9f0654083881af7d25f3b8b9ae
Anto Arun <antoarundominic@gmail.com>
2016-10-23 11:31:16 +0530

    check possible params

--- sw.js
+++ sw.js
@@ -9,10 +9,11 @@ self.addEventListener('activate', function(event) {
   console.log('sw Activated');
 });
 
-self.addEventListener('push', function(event) {
+self.addEventListener('push', function(event, payload) {
   console.log('Push message', event);
   var title = 'Push message';
   event.waitUntil(
+
     self.registration.showNotification(title, {
       'body': 'The Message',
       'icon': 'images/icon.png'
