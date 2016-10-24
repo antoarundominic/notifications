e9a889d80d9eac734fcd36985df171994ef94192
Anto arun dominic <antoarundominic@gmail.com>
2016-10-24 19:44:49 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -21,3 +21,22 @@ self.addEventListener('push', function(event, payload) {
       'icon': 'images/icon.png'
     }));
 });
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click: ', event.notification.tag);
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url == '/' && 'focus' in client)
+        return client.focus();
+    }
+    if (clients.openWindow)
+      return clients.openWindow('/');
+  }));
+});
